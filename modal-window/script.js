8d95768b3fcd3edcde01b1264eba8bfdeaa3d90d
Kerija E. Baumane <kerija.baumane@gmail.com>
2023-12-16 01:55:59 +0200

    Wrote functions for opening and closing the modal window

--- modal-window/script.js
+++ modal-window/script.js
@@ -1,1 +1,34 @@
-'use strict';
+"use strict";
+
+//  This is a Ui component
+// No matter which of these 3 buttons I click, it will open this modal window
+// To close it, i can click on the close button or click outside of the modal window, or by clicking the escape key
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const closeModalBtn = document.querySelector(".close-modal");
+const showModalBtn = document.querySelectorAll(".show-modal");
+
+// FOR LOOP FOR OPENING THE MODAL WINDOW
+for (let i = 0; i < showModalBtn.length; i++) {
+  showModalBtn[i].addEventListener("click", function () {
+    // We use classList.remove without the dot because it's not a query selector.
+    // The dot is only needed when selecting the class with querySelector; here, we are directly passing the class name for removal.
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+}
+
+// FUNCTION FOR CLOSING THE MODAL WINDOW
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+// Closing the modal with the X Button
+closeModalBtn.addEventListener("click", closeModal);
+
+// Closing the modal with the click on the overlay
+overlay.addEventListener("click", closeModal);
+
+// Closing the modal with the ESC key
