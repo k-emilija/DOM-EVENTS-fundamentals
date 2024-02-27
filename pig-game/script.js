ec045f0abb9848a62ec67a482c1d2e45c11ce565
Kerija E. Baumane <kerija.baumane@gmail.com>
2024-02-27 19:38:28 +0200

    Implemented switching players functionality

--- pig-game/script.js
+++ pig-game/script.js
@@ -12,12 +12,21 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+
 // Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
+// Defined Current Score outside the function
 let currentScore = 0;
+
+// Active Player - we start with player 0, because we are keeping scores in an array
+// Player 1 is stored in the 0 position, and the Player 2 is stored in the 1st
+const scores = [0, 0];
+let activePlayer = 0;
 
 // Rolling dice fuctionality
 btnRoll.addEventListener("click", function () {
@@ -28,10 +37,18 @@ btnRoll.addEventListener("click", function () {
   diceEl.classList.remove("hidden");
   diceEl.src = `dice-${dice}.png`;
 
-  // 3. Check for rolled 1: if dice == 1, witch to next player
+  // 3. Check for rolled 1
   if (dice !== 1) {
+    // Add dice number to currect score
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
+    // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
   }
 });
