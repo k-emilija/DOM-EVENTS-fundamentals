354c7de3767ceceac13ca0442ff73e7f1f4cbe97
Kerija E. Baumane <kerija.baumane@gmail.com>
2024-02-28 14:34:35 +0200

    Refactored the code to create a function that will initialize the game

--- pig-game/script.js
+++ pig-game/script.js
@@ -15,21 +15,31 @@ const btnHold = document.querySelector(".btn--hold");
 const player0El = document.querySelector(".player--0");
 const player1El = document.querySelector(".player--1");
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
+// FUNCTION THAT WILL INITIALIZE THE GAME
+const init = function () {
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
 
-// Defined Current Score outside the function
-let currentScore = 0;
+  diceEl.classList.add("hidden");
 
-// Active Player - we start with player 0, because we are keeping scores in an array
-// Player 1 is stored in the 0 position, and the Player 2 is stored in the 1st
-const scores = [0, 0];
-let activePlayer = 0;
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
 
-// Variable that tells if game is still active or not (with boolean)
-let playing = true;
+  // Defined Current Score outside the function
+  let currentScore = 0;
+
+  // Active Player - we start with player 0, because we are keeping scores in an array
+  // Player 1 is stored in the 0 position, and the Player 2 is stored in the 1st
+  const scores = [0, 0];
+  let activePlayer = 0;
+
+  // Variable that tells if game is still active or not (with boolean)
+  let playing = true;
+};
 
 // SWITCHING PLAYERS FUNCTIONALITY
 const switchPlayer = function () {
@@ -95,14 +105,4 @@ btnHold.addEventListener("click", function () {
 });
 
 // RESETTING THE GAME
-btnNew.addEventListener("click", function () {
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  current0El.textContent = 0;
-  current1El.textContent = 0;
-
-  player0El.classList.remove("player--winner");
-  player1El.classList.remove("player--winner");
-  player0El.classList.add("player--active");
-  player1El.classList.remove("player--active");
-});
+btnNew.addEventListener("click", function () {});
